f3e136a6e312b3dc28394d8e8e980ef880e6ac13
Altyd <francotroyd@gmail.com>
2023-09-16 21:20:24 +0200

    Massive Changes

--- Wesualize/Wesualize/Form1.Designer.cs
+++ Wesualize/Wesualize/Form1.Designer.cs
@@ -28,15 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.siticoneButton1 = new ns1.SiticoneButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.siticoneDragControl1 = new ns1.SiticoneDragControl(this.components);
+            this.siticoneButton2 = new ns1.SiticoneButton();
+            this.siticoneSlider1 = new ns1.SiticoneSlider();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.siticoneTextBox1 = new ns1.SiticoneTextBox();
+            this.siticoneButton3 = new ns1.SiticoneButton();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -46,61 +57,219 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(248, 236);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(5, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(19, 18);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 231);
+            this.button1.Location = new System.Drawing.Point(577, 123);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.Size = new System.Drawing.Size(139, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "OpenCSV File";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 258);
+            this.button2.Location = new System.Drawing.Point(577, 152);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.Size = new System.Drawing.Size(139, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "Show Data";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(68, 12);
+            this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.chart1.BackSecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.chart1.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.chart1.BorderSkin.BackSecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.chart1.BorderSkin.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            chartArea7.AxisX.LabelStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            chartArea7.AxisX.TitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            chartArea7.AxisX2.TitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            chartArea7.AxisY.LabelStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            chartArea7.AxisY.TitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            chartArea7.AxisY2.TitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            chartArea7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            chartArea7.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea7.BorderColor = System.Drawing.Color.White;
+            chartArea7.CursorX.SelectionColor = System.Drawing.Color.White;
+            chartArea7.CursorY.SelectionColor = System.Drawing.Color.White;
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(4, 22);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(503, 185);
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(712, 185);
             this.chart1.TabIndex = 4;
             this.chart1.Text = "chart1";
+            this.chart1.Visible = false;
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            this.siticoneButton1.CustomBorderThickness = new System.Windows.Forms.Padding(1);
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(28, 230);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(113, 22);
+            this.siticoneButton1.TabIndex = 5;
+            this.siticoneButton1.Text = "Load File";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, -7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(715, 30);
+            this.panel1.TabIndex = 6;
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this.panel1;
+            // 
+            // siticoneButton2
+            // 
+            this.siticoneButton2.CheckedState.Parent = this.siticoneButton2;
+            this.siticoneButton2.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            this.siticoneButton2.CustomBorderThickness = new System.Windows.Forms.Padding(1);
+            this.siticoneButton2.CustomImages.Parent = this.siticoneButton2;
+            this.siticoneButton2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.siticoneButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            this.siticoneButton2.HoveredState.Parent = this.siticoneButton2;
+            this.siticoneButton2.Location = new System.Drawing.Point(28, 258);
+            this.siticoneButton2.Name = "siticoneButton2";
+            this.siticoneButton2.ShadowDecoration.Parent = this.siticoneButton2;
+            this.siticoneButton2.Size = new System.Drawing.Size(113, 22);
+            this.siticoneButton2.TabIndex = 7;
+            this.siticoneButton2.Text = "Load Data";
+            this.siticoneButton2.Click += new System.EventHandler(this.siticoneButton2_Click);
+            // 
+            // siticoneSlider1
+            // 
+            this.siticoneSlider1.Location = new System.Drawing.Point(436, 230);
+            this.siticoneSlider1.Name = "siticoneSlider1";
+            this.siticoneSlider1.Size = new System.Drawing.Size(280, 60);
+            this.siticoneSlider1.TabIndex = 8;
+            this.siticoneSlider1.Scroll += new System.EventHandler(this.siticoneSlider1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(453, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Multiplier:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(523, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "00%";
+            // 
+            // siticoneTextBox1
+            // 
+            this.siticoneTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            this.siticoneTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneTextBox1.DefaultText = "Amount of months to predict";
+            this.siticoneTextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneTextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneTextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox1.DisabledState.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.siticoneTextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox1.FocusedState.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            this.siticoneTextBox1.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox1.HoveredState.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.Location = new System.Drawing.Point(147, 259);
+            this.siticoneTextBox1.Name = "siticoneTextBox1";
+            this.siticoneTextBox1.PasswordChar = '\0';
+            this.siticoneTextBox1.PlaceholderText = "";
+            this.siticoneTextBox1.SelectedText = "";
+            this.siticoneTextBox1.ShadowDecoration.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.Size = new System.Drawing.Size(177, 22);
+            this.siticoneTextBox1.TabIndex = 11;
+            this.siticoneTextBox1.Click += new System.EventHandler(this.siticoneTextBox1_Click);
+            this.siticoneTextBox1.Enter += new System.EventHandler(this.siticoneTextBox1_Enter);
+            // 
+            // siticoneButton3
+            // 
+            this.siticoneButton3.CheckedState.Parent = this.siticoneButton3;
+            this.siticoneButton3.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            this.siticoneButton3.CustomBorderThickness = new System.Windows.Forms.Padding(1);
+            this.siticoneButton3.CustomImages.Parent = this.siticoneButton3;
+            this.siticoneButton3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
+            this.siticoneButton3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            this.siticoneButton3.HoveredState.Parent = this.siticoneButton3;
+            this.siticoneButton3.Location = new System.Drawing.Point(147, 230);
+            this.siticoneButton3.Name = "siticoneButton3";
+            this.siticoneButton3.ShadowDecoration.Parent = this.siticoneButton3;
+            this.siticoneButton3.Size = new System.Drawing.Size(177, 22);
+            this.siticoneButton3.TabIndex = 12;
+            this.siticoneButton3.Text = "Export Predictions";
+            this.siticoneButton3.Click += new System.EventHandler(this.siticoneButton3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(32)))));
             this.ClientSize = new System.Drawing.Size(717, 293);
+            this.Controls.Add(this.siticoneButton3);
+            this.Controls.Add(this.siticoneTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.chart1);
+            this.Controls.Add(this.siticoneSlider1);
+            this.Controls.Add(this.siticoneButton2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.siticoneButton1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(176)))), ((int)(((byte)(167)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Wesualize";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +281,15 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private ns1.SiticoneButton siticoneButton1;
+        private System.Windows.Forms.Panel panel1;
+        private ns1.SiticoneDragControl siticoneDragControl1;
+        private ns1.SiticoneButton siticoneButton2;
+        private ns1.SiticoneSlider siticoneSlider1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private ns1.SiticoneTextBox siticoneTextBox1;
+        private ns1.SiticoneButton siticoneButton3;
     }
 }
 
--- Wesualize/Wesualize/Form1.cs
+++ Wesualize/Wesualize/Form1.cs
@@ -19,13 +19,49 @@ namespace Wesualize
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
         private void btnOpenFile_Click(object sender, EventArgs e)
         {
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+ 
+        }
+        private List<DataEntry> dataEntries = new List<DataEntry>();
+
+        private void btnShowGraph_Click(object sender, EventArgs e)
+        {
+        }
+
+        private class DataEntry
+        {
+            public string Company { get; set; }
+            public string Date { get; set; }
+            public int Sales { get; set; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        public class SalesData
+        {
+            public float MonthNumber { get; set; }
+            public float Sales { get; set; }
+        }
+
+        public class SalesPrediction
+        {
+            public float Score { get; set; }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
         {
             openFileDialog1.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
@@ -43,7 +79,6 @@ namespace Wesualize
                             Company = values[0].Trim(),
                             Date = values[1].Trim(),
                             Sales = int.Parse(values[2].Trim()),
-                            Views = int.Parse(values[3].Trim())
                         });
                     }
 
@@ -55,21 +90,18 @@ namespace Wesualize
                 }
             }
         }
-        private List<DataEntry> dataEntries = new List<DataEntry>();
-
-        private void btnShowGraph_Click(object sender, EventArgs e)
+        public enum Bias
         {
+            Positive,
+            Neutral,
+            Negative
         }
-
-        private class DataEntry
+        private void siticoneButton2_Click(object sender, EventArgs e)
         {
-            public string Company { get; set; }
-            public string Date { get; set; }
-            public int Sales { get; set; }
-            public int Views { get; set; }
+            // Call the overloaded method with default Bias.Neutral
+            siticoneButton2_Click(sender, e, Bias.Negative);
         }
-
-        private void button2_Click(object sender, EventArgs e)
+        private void siticoneButton2_Click(object sender, EventArgs e, Bias bias)
         {
             chart1.Series.Clear();
 
@@ -89,36 +121,113 @@ namespace Wesualize
                     series.Points.AddXY(companyData[i].Date, companyData[i].Sales);
                 }
 
-                // Set up ML.NET
-                var context = new MLContext();
+                // Set up ML.NET with a fixed seed for consistency
+                var context = new MLContext(seed: 0);
                 var trainData = context.Data.LoadFromEnumerable(salesData);
                 var pipeline = context.Transforms.Concatenate("Features", "MonthNumber")
                     .Append(context.Regression.Trainers.Sdca(labelColumnName: "Sales", maximumNumberOfIterations: 100));
 
                 var model = pipeline.Fit(trainData);
-
-                // Predict next 3 months
-                for (int i = 1; i <= 3; i++)
+                float y;
+                int num = int.Parse(siticoneTextBox1.Text);
+                // Predict next 2 months
+                for (int i = 1; i <= (num); i++)
                 {
                     var prediction = model.Transform(context.Data.LoadFromEnumerable(new List<SalesData> { new SalesData { MonthNumber = companyData.Count + i } }));
                     var salesPrediction = context.Model.CreatePredictionEngine<SalesData, SalesPrediction>(model).Predict(new SalesData { MonthNumber = companyData.Count + i });
+                    string before = siticoneSlider1.Value.ToString();
+                    float xx = float.Parse(before);
+                    y = xx / 10;
+                    float amount = y / 10;
+                    switch (bias)
+                    {
+
+                        case Bias.Positive:
+                            float amount3 = 1 + amount;
+                            Console.WriteLine(amount3);
+                            if (siticoneSlider1.Value == 0)
+                            {
+                                salesPrediction.Score = (float)(salesPrediction.Score * 1.05);  // Increase by 5%
+                            }
+                            else
+                            {
+                                salesPrediction.Score = (float)(salesPrediction.Score * amount3);  // Incrase by 5%
+                            }
+                            break;
+                        case Bias.Negative:
+                            float amount2 = 1 - amount;
+                            Console.WriteLine(amount2);
+                            if (siticoneSlider1.Value == 0)
+                            {
+                                salesPrediction.Score = (float)(salesPrediction.Score * 0.95);  // Decrease by 5%
+                            }
+                            else
+                            {
+                                salesPrediction.Score = (float)(salesPrediction.Score * amount2);  // Decrease by 5%
+                            }
+                            break;
+                            // Neutral does nothing
+                    }
+
                     series.Points.AddXY($"Month {companyData.Count + i}", salesPrediction.Score);
                 }
 
                 chart1.Series.Add(series);
+                chart1.Visible = true;
             }
         }
-        public class SalesData
+
+        private void siticoneSlider1_Scroll(object sender, EventArgs e)
         {
-            public float MonthNumber { get; set; }
-            public float Sales { get; set; }
+            string before = siticoneSlider1.Value.ToString();
+            float x = float.Parse(before);
+            float y = x / 10;
+            label3.Text = y.ToString() + "%";
         }
 
-        public class SalesPrediction
+        private void siticoneTextBox1_Click(object sender, EventArgs e)
         {
-            public float Score { get; set; }
+            siticoneTextBox1.Text = "";
         }
 
+        private void siticoneTextBox1_Enter(object sender, EventArgs e)
+        {
+            siticoneTextBox1.Text = "";
+        }
+
+        private void siticoneButton3_Click(object sender, EventArgs e)
+        {
+            if (chart1.Series.Count == 0)
+            {
+                MessageBox.Show("No predictions to export. Please generate predictions first.");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                List<string> csvData = new List<string>
+        {
+            "Company,Date,SalesPrediction" // header
+        };
+
+                foreach (var series in chart1.Series)
+                {
+                    for (int i = 0; i < series.Points.Count; i++)
+                    {
+                        string company = series.Name;
+                        string date = series.Points[i].AxisLabel;
+                        string prediction = series.Points[i].YValues[0].ToString("F2");
+
+                        csvData.Add($"{company},{date},{prediction}");
+                    }
+                }
+
+                File.WriteAllLines(saveFileDialog.FileName, csvData);
+                MessageBox.Show("Predictions exported successfully.");
+            }
+        }
     }
 }
 
